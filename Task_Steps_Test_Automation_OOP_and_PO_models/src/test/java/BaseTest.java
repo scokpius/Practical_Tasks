78f81c277985b9ac8ffc27fd51a9f8cec57b4ab4
rodionfisenko <minsk@joingo.com>
2021-10-29 15:32:08 +0300

    refactoring update jenkins

--- Task_Steps_Test_Automation_OOP_and_PO_models/src/test/java/BaseTest.java
+++ Task_Steps_Test_Automation_OOP_and_PO_models/src/test/java/BaseTest.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class BaseTest {
     @BeforeAll
     public static void setUp(){
-        Configuration.remote = "http://Localhost:4444/wd/hub";
+        Configuration.remote = "http://selenide:4444/wd/hub";
         Configuration.browser = "chrome";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
