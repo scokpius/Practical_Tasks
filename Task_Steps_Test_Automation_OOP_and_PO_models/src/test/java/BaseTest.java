41115e6be69d6eb3ffab0435d3598846d871ca23
rodionfisenko <minsk@joingo.com>
2021-10-29 16:42:03 +0300

    refactoring update jenkins

--- Task_Steps_Test_Automation_OOP_and_PO_models/src/test/java/BaseTest.java
+++ Task_Steps_Test_Automation_OOP_and_PO_models/src/test/java/BaseTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
@@ -9,21 +10,21 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 public class BaseTest {
     @BeforeAll
     public static void setUp(){
-        Configuration.remote = "http://selenoid:4444/wd/hub";
+       Configuration.remote = "http://selenoid:4444/wd/hub";
+      //  Configuration.remote = "http://localhost:4444/wd/hub";
         Configuration.browser = "chrome";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
 
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("incognito");
-//        Configuration.browserCapabilities = new DesiredCapabilities();
-//        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
-//        Configuration.startMaximized = true;
-//        Configuration.timeout = 15000;
-//        Configuration.holdBrowserOpen = true;
-//        Configuration.headless = false;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("incognito");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        Configuration.startMaximized = true;
+        Configuration.timeout = 15000;
+        Configuration.holdBrowserOpen = true;
+        Configuration.headless = false;
     }
 
     @AfterAll
